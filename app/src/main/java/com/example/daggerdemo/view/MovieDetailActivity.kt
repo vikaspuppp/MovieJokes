dfbc4ba12a4fb2dabbb8a214023053a8339bbe14
vikas_prasad <vikas.prasad@publicissapient.com>
2020-06-21 19:42:25 +0530

    mvvm pattern introduce in movie detail activity

--- app/src/main/java/com/example/daggerdemo/view/MovieDetailActivity.kt
+++ app/src/main/java/com/example/daggerdemo/view/MovieDetailActivity.kt
@@ -5,26 +5,36 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.text.method.ScrollingMovementMethod
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import com.example.daggerdemo.R
-import com.example.daggerdemo.model.DataTransfer
 import com.example.daggerdemo.model.MovieModel
+import com.example.daggerdemo.viewmodel.MovieDetailViewModel
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_movie_detail.*
 
 class MovieDetailActivity : AppCompatActivity() {
-    private lateinit var model: MovieModel
+    private val viewMovieModel: MovieDetailViewModel by viewModels()
     @TargetApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_detail)
-        model = DataTransfer.get() as MovieModel
-        setData();
+        setData()
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
     private fun setData() {
+        viewMovieModel.getMovieData().observe(this,
+            Observer<MovieModel> {
+                setAnswer(it)
+            })
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun setAnswer(model: MovieModel) {
         tv_name.text = model.name
         tv_first_appearence.text =
             "Published by ${model.publisher} in ${model.firstAppearance} as team of ${model.team} created by ${model.createdBy}"
--- app/src/main/java/com/example/daggerdemo/viewmodel/MovieDetailViewModel.kt
+++ app/src/main/java/com/example/daggerdemo/viewmodel/MovieDetailViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.daggerdemo.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.daggerdemo.model.DataTransfer
+import com.example.daggerdemo.model.MovieModel
 
 class MovieDetailViewModel : ViewModel() {
+
+    private val movieData: MutableLiveData<MovieModel> by lazy { loadData() }
+
+    private fun loadData(): MutableLiveData<MovieModel> =
+        MutableLiveData<MovieModel>().also { it.value = DataTransfer.get() as MovieModel }
+
+    fun getMovieData(): LiveData<MovieModel> = movieData
 }
